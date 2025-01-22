35057fb8fe5295e9b1aa41b0f81d8bcf82393384
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-23 01:13:40 +0300

    changed ui system and added generateButton

--- app/src/main/java/com/bcan/aitattoogenerator/presentation/generate/GenerateScreen.kt
+++ app/src/main/java/com/bcan/aitattoogenerator/presentation/generate/GenerateScreen.kt
@@ -1,19 +1,10 @@
 package com.bcan.aitattoogenerator.presentation.generate
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,18 +12,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.bcan.aitattoogenerator.presentation.generate.components.DescribeTattooTextField
-import com.bcan.aitattoogenerator.presentation.generate.components.SelectStyleItem
-import com.bcan.aitattoogenerator.presentation.ui.TitleText
-import com.bcan.aitattoogenerator.ui.theme.White
-import com.bcan.aitattoogenerator.ui.theme.getProButtonGradient
+import com.bcan.aitattoogenerator.presentation.generate.components.GenerateButton
+import com.bcan.aitattoogenerator.presentation.generate.sections.AdvancedDesignSection
+import com.bcan.aitattoogenerator.presentation.generate.sections.SelectStyleSection
+import com.bcan.aitattoogenerator.presentation.generate.sections.TitleAndGetProSection
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun GenerateScreen() {
 
@@ -43,42 +29,21 @@ fun GenerateScreen() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TitleText(text = "Describe Your Tattoo".uppercase())
-            Surface(
-                color = Color.Transparent,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(getProButtonGradient)
-                    .clickable { }
-            ) {
-                Text(
-                    text = "Get Pro".uppercase(),
-                    modifier = Modifier.padding(vertical = 3.dp, horizontal = 10.dp),
-                    color = White,
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold, letterSpacing = 1.2.sp
-                )
-            }
-        }
+        TitleAndGetProSection(onGetProClick = {})
 
         DescribeTattooTextField(
             text = describeTattooText,
             onValueChange = { describeTattooText = it }
         )
 
-        TitleText(text = "Select Style".uppercase())
-        FlowRow(
-            modifier = Modifier.horizontalScroll(rememberScrollState()),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        SelectStyleSection()
+        AdvancedDesignSection()
+
+        Box(
+            modifier = Modifier.fillMaxHeight(),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            repeat(10) {
-                SelectStyleItem()
-            }
+            GenerateButton(onClick = {})
         }
     }
 }
