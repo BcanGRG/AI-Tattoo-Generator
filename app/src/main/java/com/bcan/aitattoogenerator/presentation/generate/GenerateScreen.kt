893172787b5a01c4205ebac241a9d2d62472f61f
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-22 16:05:36 +0300

    added DescribeTattooTextField in GenerateScreen

--- app/src/main/java/com/bcan/aitattoogenerator/presentation/generate/GenerateScreen.kt
+++ app/src/main/java/com/bcan/aitattoogenerator/presentation/generate/GenerateScreen.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,13 +22,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bcan.aitattoogenerator.presentation.generate.components.DescribeTattooTextField
 import com.bcan.aitattoogenerator.presentation.ui.TitleText
 import com.bcan.aitattoogenerator.ui.theme.White
 import com.bcan.aitattoogenerator.ui.theme.getProButtonGradient
 
 @Composable
 fun GenerateScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
+
+    var describeTattooText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
 
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -48,6 +59,12 @@ fun GenerateScreen() {
                 )
             }
         }
+
+        DescribeTattooTextField(
+            text = describeTattooText,
+            onValueChange = { describeTattooText = it }
+        )
+
         TitleText(text = "Select Style".uppercase())
     }
 }
