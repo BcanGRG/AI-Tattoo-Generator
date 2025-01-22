f16aa0eccf18b6c5f6c0cb515c340bbf7abbb8d6
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-23 01:14:02 +0300

    added new colors

--- app/src/main/java/com/bcan/aitattoogenerator/ui/theme/Color.kt
+++ app/src/main/java/com/bcan/aitattoogenerator/ui/theme/Color.kt
@@ -12,8 +12,10 @@ val Pink40 = Color(0xFF7D5260)
 
 val Black = Color(0xFF000000)
 val CodGray = Color(0xFF111111) // Background Color
+val AbbeyGray = Color(0xFF48484A)
 val White = Color(0xFFFFFFFF)
-val CharlestonGreen = Color(0xFF2A2A2A)// Advanced Design Background
+val Tundora = Color(0xFF2A2A2A)// Advanced Design Background
+val Manatee = Color(0xFF8E8E93)
 val EerieBlack = Color(0xFF1E1E1E)
 val SmokeyBlack = Color(0xFF0E0E0D) // BottomBar Color
 val AzureRadiance = Color(0xFF0A84FF) // Selected Colors
@@ -21,3 +23,5 @@ val Boulder = Color(0xFF7C7C7C) // UnSelected Colors
 
 val RoyalBlue = Color(0xFF6A35E7) // GetPro gradient
 val Heliotrope = Color(0xFFAA73FB) // GetPro gradient
+val Heliotro = Color(0xFFBF5AF2)
+val CornflowerBlue = Color(0xFF8175FA)
