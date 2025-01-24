060cef8f466aed8f84c931c7c9567d04a28ab700
burak.gorgulu <burak.gorgulu@beymen.com>
2025-01-24 09:03:54 +0300

    added BottomSheet structure

--- app/src/main/java/com/bcan/aitattoogenerator/presentation/utils/navigation/AITattooGeneratorNavigation.kt
+++ app/src/main/java/com/bcan/aitattoogenerator/presentation/utils/navigation/AITattooGeneratorNavigation.kt
@@ -3,9 +3,11 @@ package com.bcan.aitattoogenerator.presentation.utils.navigation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -13,9 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.bcan.aitattoogenerator.ui.theme.CodGray
+import com.bcan.aitattoogenerator.presentation.ui.sheets.BottomSheetHost
+import com.bcan.aitattoogenerator.ui.theme.Black11
 import com.bcan.aitattoogenerator.ui.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AiTattooGeneratorNavigation() {
     val navController = rememberNavController()
@@ -23,6 +27,9 @@ fun AiTattooGeneratorNavigation() {
     val currentDestination = entry?.destination
 
     val snackbarHostState = remember { SnackbarHostState() }
+    val sheetState = rememberModalBottomSheetState()
+
+    BottomSheetHost(sheetState = sheetState)
 
     Scaffold(
         bottomBar = { AiTattooGeneratorBottomBar(currentDestination, navController) },
@@ -31,7 +38,7 @@ fun AiTattooGeneratorNavigation() {
     ) {
         NavHost(
             modifier = Modifier
-                .background(CodGray)
+                .background(Black11)
                 .padding(it)
                 .fillMaxSize(),
             navController = navController,
